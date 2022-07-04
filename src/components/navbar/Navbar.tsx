59c0939868c1d5ab34734a2f80ba572c1a7cbd90
nikshama <nika.shamanauri@gmail.com>
2022-07-04 15:52:07 +0400

    დოტას ლოგო და favicon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,18 @@
 import DarkModeToggler from "./darkmode-toggler/DarkModeToggler";
 import SearchFrom from "./search/SearchForm";
 
+import logo from "../../assets/images/logo.png";
+
 function Navbar() {
     return (
         <nav className="bg-slate-50 py-3 dark:bg-neutral-800">
             <div className="container mx-auto flex justify-between">
                 <div className="flex items-center">
-                    <img
-                        className="mr-2 w-6"
-                        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-                        alt="logo"
-                    />
+                    <img className="mr-2 w-5" src={logo} alt="logo" />
                     <span>Dota 2 - AI</span>
                 </div>
                 <SearchFrom />
                 <div className="flex items-center space-x-5">
-                    {/* <svg className="h-5 w-5" viewBox="0 0 448 512">
-                        <path
-                            className="fill-current"
-                            d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM224 32c52.94 0 96 43.06 96 96c0 52.93-43.06 96-96 96S128 180.9 128 128C128 75.06 171.1 32 224 32zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM413.3 480H34.66C33.2 480 32 478.8 32 477.3C32 399.4 95.4 336 173.3 336h101.3C352.6 336 416 399.4 416 477.3C416 478.8 414.8 480 413.3 480z"
-                        />
-                    </svg> */}
                     <DarkModeToggler />
                 </div>
             </div>
